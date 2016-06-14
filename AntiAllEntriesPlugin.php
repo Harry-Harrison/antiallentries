a0cd37e233666e31d1704894ca3aca9fe9c44851
Harry Harrison <h@rryharrison.co>
2016-06-14 12:32:09 +0100

    Init
    Shouldn’t need much more than this…

---
+++ AntiAllEntriesPlugin.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Anti-All Entries plugin for Craft CMS
+ *
+ * Removes the All Entries item from Craft's Entries view.
+ *
+ *
+ * @author    Harry Harrison
+ * @copyright Copyright (c) 2016 Harry Harrison
+ * @link      http://harryharrison.co/
+ * @package   AntiallEntries
+ * @since     1.0.0
+ */
+
+namespace Craft;
+
+class AntiAllEntriesPlugin extends BasePlugin
+{
+	public function getName()
+	{
+		return Craft::t('Anti-All Entries');
+	}
+
+    public function getDescription()
+    {
+        return Craft::t('Removes the All Entries item from Craft\'s Entries view.');
+    }
+
+    public function getDocumentationUrl()
+    {
+        return 'https://github.com/harry-harrison/antiallentries/blob/master/README.md';
+    }
+
+    public function getReleaseFeedUrl()
+    {
+        return 'https://raw.githubusercontent.com/harry-harrison/antiallentries/master/releases.json';
+    }
+
+	public function getVersion()
+	{
+		return '1.0.0';
+	}
+
+    public function getSchemaVersion()
+    {
+        return '1.0.0';
+    }
+
+	public function getDeveloper()
+	{
+		return 'Harry Harrison';
+	}
+
+	public function getDeveloperUrl()
+	{
+		return 'http://harryharrison.co';
+	}
+
+    public function hasCpSection()
+    {
+        return false;
+    }
+
+    public function modifyEntrySources(&$sources, $context)
+    {
+        if ($context == 'index')
+        {
+            unset($sources['*']);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# Anti-All Entries plugin for Craft CMS
+
+Removes the All Entries item from Craft's Entries view.
+
+## Installation
+
+To install Anti-All Entries, follow these steps:
+
+1. Download & unzip the file and place the `antiallentries` directory into your `craft/plugins` directory
+2.  -OR- do a `git clone https://github.com/harry-harrison/antiallentries.git` directly into your `craft/plugins` folder.  You can then update it with `git pull`
+3. Install plugin in the Craft Control Panel under Settings > Plugins
+4. The plugin folder should be named `antiallentries` for Craft to see it.  GitHub recently started appending `-master` (the branch name) to the name of the folder for zip file downloads.
+
+Anti-All Entries works on Craft 2.4.x and Craft 2.5.x.
+
+## Anti-All Entries Changelog
+
+### 1.0.0 -- 2016.06.14
+
+* Initial release
+
+Brought to you by [Harry Harrison](http://harryharrison.co/)
